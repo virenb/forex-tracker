5abf4a3af24442dcb39cf7c5284477e5326388e1
Viren Bhagat <virenb@gmail.com>
2017-07-05 14:42:13 -0400

    Added reactstrap and some styling

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import 'bootstrap/dist/css/bootstrap.css';
 require('./style.css');
 
 render(<App />, document.getElementById('app'));
